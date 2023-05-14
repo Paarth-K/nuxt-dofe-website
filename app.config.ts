9a232f0a37563e4aab203365039f3e372c28e365
Paarth Kukreja <paarthkukreja@outlook.com>
2023-05-14 23:44:45 +0800

    moved icons up a bit for aliginment purposes

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+// app.config.ts
+export default defineAppConfig({
+  nuxtIcon: {
+    size: "19px", // default <Icon> size applied
+    class: "icon", // default <Icon> class applied
+    aliases: {
+      nuxt: "logos:nuxt-icon",
+    },
+  },
+});

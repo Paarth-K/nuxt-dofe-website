aaafd4015b9e8f56659342424d8f30e39df4d9b0
Paarth Kukreja <paarthkukreja@outlook.com>
2023-05-15 23:08:59 +0800

    lang en

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
         "https://www.paarthkukrejadofe.eu.org",
     },
   },
+
   image: {
     imagekit: {
       baseURL: "https://ik.imagekit.io/1vyqdxrhm/",
@@ -35,6 +36,9 @@ export default defineNuxtConfig({
           children: `(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "gnxxzdn7ib");`,
         },
       ],
+      htmlAttrs: {
+        lang: "en",
+      },
     },
   },
 

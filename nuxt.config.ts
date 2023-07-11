56a878375a5b180e0bce19338d2e096f2a86edff
Paarth Kukreja <paarthkukreja@outlook.com>
2023-07-11 22:34:31 +0530

    changed site url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,8 +22,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       siteUrl:
-        process.env.NUXT_PUBLIC_SITE_URL ||
-        "https://www.paarthkukrejadofe.eu.org",
+        process.env.NUXT_PUBLIC_SITE_URL || "https://dofe.paarthkukreja.com",
     },
   },
   routeRules: {
